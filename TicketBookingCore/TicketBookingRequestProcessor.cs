d767c882cfab57595e07395b91c3531962c2ab46
mesri <abde_mes@yahoo.fr>
2023-06-19 13:36:01 +0200

    refactor mapping object in TicketBookingCore/TicketBookingRequestProcessor.cs

--- TicketBookingCore/TicketBookingRequestProcessor.cs
+++ TicketBookingCore/TicketBookingRequestProcessor.cs
@@ -17,14 +17,15 @@ namespace TicketBookingCore
                 throw new ArgumentNullException(nameof(request));
             }
 
-            _ticketBookingRepository.Save(new TicketBooking
-            {
-                FirstName = request.FirstName,
-                LastName = request.LastName,
-                Email = request.Email
-            });
+            _ticketBookingRepository.Save(Create<TicketBooking>(request));
 
-            return new TicketBookingResponse
+            return Create<TicketBookingResponse>(request);
+        }
+
+
+        private static T Create<T>(TicketBookingRequest request) where T : TicketBookingBase, new()
+        {
+            return new T
             {
                 FirstName = request.FirstName,
                 LastName = request.LastName,
